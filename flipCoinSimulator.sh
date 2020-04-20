87b0b59443fa221adbf75269ae1eb6e39e1d6539
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-20 08:24:38 +0530

    flip through a loop  uc is added

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,20 @@
 #! /bin/bash -x
-#simulate coin flip
+#flip teh coin through a loop
 
-flip=$(($RANDOM%2))
-if [ $flip -eq 1 ];
-then
-	echo "you got heads-winner"
-else
-        echo "you got tails-looser"
-fi
+HEAD=0
+TAIL=0
+while [ $HEAD -lt 11 ] && [ $TAIL -lt 11 ]
+do
+        toss=$(($RANDOM%3))
+
+        if [ $toss -eq 1 ]; then
+                echo "heads"
+                HEAD=$(($HEAD+1))
+        else
+                echo "tails"
+                TAIL=$(($TAIL+1))
+        fi
+done
+
+echo "you got $HEAD heads and $TAIL tails in flip"
+
