5287ac62220dded6cb43cd7ae2b81c6de2860dd0
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-20 08:33:42 +0530

    added who won uc

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,20 +1,28 @@
 #! /bin/bash -x
 #flip teh coin through a loop
 
-HEAD=0
-TAIL=0
-while [ $HEAD -lt 11 ] && [ $TAIL -lt 11 ]
+HEAD_count=0
+TAIL_count=0
+while [ $HEAD_count -lt 21 ] && [ $TAIL_count -lt 21 ]
 do
         toss=$(($RANDOM%3))
 
         if [ $toss -eq 1 ]; then
                 echo "heads"
-                HEAD=$(($HEAD+1))
+                HEAD_count=$(($HEAD_count+1))
         else
                 echo "tails"
-                TAIL=$(($TAIL+1))
+                TAIL_count=$(($TAIL_count+1))
         fi
 done
 
-echo "you got $HEAD heads and $TAIL tails in flip"
-
+echo "you got $HEAD_count heads and $TAIL_count tails in flip"
+if [ $HEAD_count -gt $TAILS_count ];
+then
+	echo "HEAD WON BY $(($HEAD_count-$TAIL_count)) Tosses"
+elif [ $TAIL_count -gt $HEAD_count ];
+then
+	echo "TAIL WON BY $(($TAIL_count-$HEAD_count)) Tosses"
+else
+	echo "tied between head and tail"
+fi
